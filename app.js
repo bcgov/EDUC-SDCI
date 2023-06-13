1c4bb683bbee386e790d1f37fb7a36d9d5b3562e
Shaun Lum <shaun.lum@gov.bc.ca>
2023-06-13 11:00:17 -0700

    added files

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const config = require('./config/index');
+const dotenv = require('dotenv');
+const express = require("express");
+const axios = require("axios");
+const cors = require('cors');
+const NodeCache = require("node-cache");
+const apiRouter = express.Router();
+const instituteRouter = require('./routes/institute-router');
+const app = express();
+app.use(cors());
+
+app.use(/(\/api)?/, apiRouter);
+
+//institute Router
+apiRouter.use('/v1/institute', instituteRouter);
+
+
+//Handle 500 error
+app.use((err, _req, res, next) => {
+  res?.redirect(config?.get('server:frontend') + '/error?message=500_internal_error');
+  next();
+});
+
+// Handle 404 error
+app.use((_req, res) => {
+  res.redirect(config?.get('server:frontend') + '/error?message=404_Page_Not_Found');
+});
+
+// Prevent unhandled errors from crashing application
+process.on('unhandledRejection', err => {
+});
+module.exports = app;
---
+++ config/index.js
@@ -0,0 +1,41 @@
+'use strict';
+const nconf = require('nconf');
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config();
+
+const env = process.env.NODE_ENV || 'local';
+nconf.argv()
+  .env()
+  .file({ file: path.join(__dirname, `${env}.json`) });
+
+//injects environment variables into the json file
+nconf.overrides({
+  environment: env,
+
+  server: {
+    logLevel: process.env.LOG_LEVEL,
+    morganFormat: 'dev',
+    port: 8080
+  }
+});
+nconf.defaults({
+  environment: env,
+  server: {
+    frontend: process.env.SERVER_FRONTEND,
+    backend: process.env.SERVER_FRONTEND + '/api',
+    logLevel: process.env.LOG_LEVEL,
+    morganFormat: 'dev',
+    port: 8080,
+    session: {
+      maxAge: +process.env.SESSION_MAX_AGE
+    },
+    instituteAPIURL: process.env.INSTITUTE_API_URL,
+  },
+  oidc: {
+    clientId: process.env.SOAM_CLIENT_ID,
+    clientSecret: process.env.SOAM_CLIENT_SECRET,
+    discovery: process.env.SOAM_DISCOVERY
+  },
+});
+module.exports = nconf;
