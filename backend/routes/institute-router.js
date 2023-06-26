de900a9d080f6509f7b7fdb5046b91492b7458ec
Shaun Lum <shaun.lum@gov.bc.ca>
2023-06-26 11:22:26 -0700

    changed to let node-cache handle the expiry of the token

--- backend/routes/institute-router.js
+++ backend/routes/institute-router.js
@@ -5,7 +5,8 @@ const config = require('../config/index');
 const NodeCache = require("node-cache");
 const jsonwebtoken = require("jsonwebtoken");
 const axios = require("axios");
-const cache = new NodeCache({ stdTTL: 300 });
+
+const cache = new NodeCache({ stdTTL: config.get("server:instituteAPITokenExpiry") });
 const clientId = config.get("oidc:clientId");
 const clientSecret = config.get("oidc:clientSecret")
 const tokenEndpoint = config.get("oidc:tokenEndpoint")
@@ -17,68 +18,37 @@ const data = {
 };
 
 //Batch Routes
-router.get('/*',getToken, getInstituteAPI);
+router.get('/*',checkToken, getInstituteAPI);
 
-async function isTokenExpired(token) {
-  const now = Date.now().valueOf() / 1000;
-  const payload = jsonwebtoken.decode(token);
-  console.log(payload)
-  console.log("Token: " + payload['exp']) 
-  console.log("Time : " + now)
+async function getNewToken(req, res, next) {
+  await axios
 
-  return (!!payload['exp'] && payload['exp'] < (now + 30)); // Add 30 seconds to make sure , edge case is avoided and token is refreshed.
-}
-
-async function getToken(req, res, next) {
+  .post(tokenEndpoint, data, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+  .then((response) => {
+    let accessToken = response.data.access_token;
+    cache.set("token", accessToken);
+    
+  })
+  .catch((error) => {
+    console.error("Error:", error.response.data);
+  });
+} 
+async function checkToken(req, res, next) {
   try{
-    let token = await cache.get("token")
-
-    if(token && !isTokenExpired(token)){
-      console.log("TOKEN IS EXPIRED... GETTING A NEW ONE")
-      await axios
-      .post(tokenEndpoint, data, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      })
-      .then((response) => {
-        let accessToken = response.data.access_token;
-        console.log(accessToken)
-        cache.set("token", accessToken);
-        
-      })
-      .catch((error) => {
-        console.error("Error:", error.response.data);
-      });
-    }else{
-      await axios
-      .post(tokenEndpoint, data, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      })
-      .then((response) => {
-        let accessToken = response.data.access_token;
-        console.log(accessToken)
-        cache.set("token", accessToken);
-        
-      })
-      .catch((error) => {
-        console.error("Error:", error.response.data);
-      });
+    if(await !cache.has("token") ){
+      await getNewToken();
     }
     next();  
   }catch(error){
     console.log(error)
   }
-      
-  
-  
 }
 async function getInstituteAPI(req, res) {
   const memToken = await cache.get("token");  
-
-  
   const url = `${config.get('server:instituteAPIURL')}/institute` + req.url;
   axios
     .get(url, { headers: { Authorization: `Bearer ${memToken}` } })
