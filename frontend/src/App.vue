e9d1498df8bebe781e410bb0826146565b6300ed
suzalflueck <samflueck95@gmail.com>
2023-06-30 12:09:39 -0700

    Added routes to header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,6 +13,10 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/search">School Search</RouterLink>
+        <RouterLink to="/school">School</RouterLink>
+        <RouterLink to="/district">District</RouterLink>
+        <RouterLink to="/authority">Authority</RouterLink>
       </nav>
     </div>
   </header>
