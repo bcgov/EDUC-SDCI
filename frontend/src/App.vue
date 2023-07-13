491ef1649edfa1862c6b3209407da8e3d8f5b71f
Shaun Lum <shaun.lum@gov.bc.ca>
2023-07-13 09:12:32 -0700

    test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -51,6 +51,7 @@ export default {
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
