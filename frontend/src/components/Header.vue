9f9f718d5b82a08fb2764f408e2335fb2d457bc1
suzalflueck <samflueck95@gmail.com>
2023-07-31 16:20:46 -0700

    Style changes to header and commented out darkmode CSS for now

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -14,8 +14,8 @@
     </a>
     <h1>School and District Contact Information</h1>
     <!-- TODO: Move to DEBUG component? Links are temporary until we build out our views.-->
-    <div class="wrapper">
-      <nav>
+    <template v-slot:extension>
+      <div>
         <RouterLink class="mx-2" to="/">Home</RouterLink>
         <RouterLink class="mx-2" to="/about">About</RouterLink>
         <RouterLink class="mx-2" to="/search">School Search</RouterLink>
@@ -24,17 +24,16 @@
         <RouterLink class="mx-2" to="/offshore">Offshore</RouterLink>
         <RouterLink class="mx-2" to="/list-districts">List Districts</RouterLink>
         <RouterLink class="mx-2" to="/list-schools">List Schools</RouterLink>
-      </nav>
-    </div>
+      </div>
+    </template>
   </v-app-bar>
 </template>
 
 <style>
-/* TODO: Override when properly implementing app header */
-header {
-  padding-left: 60px;
-}
-.wrapper {
-  display: block;
+/* sizes header to align with content on desktop */
+.v-toolbar__content,
+.v-toolbar__extension {
+  max-width: 1280px;
+  padding: 0 2rem;
 }
 </style>
