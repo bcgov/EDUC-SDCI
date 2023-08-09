8a77065ed058b60da52f308bdbb64502be6b06b1
Tang <Michael.Tang@gov.bc.ca>
2023-08-09 15:24:57 -0700

    added functionality to school page

--- frontend/src/views/SchoolView.vue
+++ frontend/src/views/SchoolView.vue
@@ -1,5 +1,23 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>Hello World, I'm SchoolView.vue!</div>
+  <h1>Selected school data</h1>
+  <div>{{ schoolData.value }}</div>
 </template>
+<script setup>
+import { reactive, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import InstituteService from '@/Services/InstituteService'
+
+const schoolData = reactive({ value: {} })
+onMounted(async () => {
+  const route = useRoute()
+  const selectedSchoolId = route.params.schoolId
+
+  try {
+    const response = await InstituteService.getSchool(selectedSchoolId)
+    schoolData.value = response.data
+  } catch (error) {
+    console.error(error)
+  }
+})
+</script>
+<style></style>
