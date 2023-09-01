9fc9890d8a921fe6de6c20c36630b8891270057f
Shaun Lum <51839198+shaunlumbcgov@users.noreply.github.com>
2023-09-01 16:26:46 -0700

    Update update-configmap.sh

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -26,9 +26,14 @@ fi
 
 echo Creating config map $APP_NAME-backend-config-map
 oc create -n $OPENSHIFT_NAMESPACE-$envValue configmap $APP_NAME-backend-config-map --from-literal=TZ=$TZVALUE --from-literal=BANNER_COLOR=$bannerColor --from-literal=BANNER_ENVIRONMENT=$bannerEnvironment --from-literal=SOAM_CLIENT_ID=$KC_CLIENT_SECRET --from-literal=SOAM_CLIENT_SECRET=$KC_CLIENT_ID --from-literal=SERVER_FRONTEND="$SERVER_FRONTEND" --from-literal=INSTITUTE_API_ENDPOINT="http://institute-api-master.$COMMON_NAMESPACE-$envValue.svc.cluster.local:8080/api/v1/institute" --from-literal=LOG_LEVEL=info  --from-literal=NODE_ENV="openshift" --dry-run -o yaml | oc apply -f -
+echo
+echo Setting environment variables for $APP_NAME-backend-$SOAM_KC_REALM_ID application
+oc -n $OPENSHIFT_NAMESPACE-$envValue set env --from=configmap/$APP_NAME-backend-config-map dc/$APP_NAME-backend-$SOAM_KC_REALM_ID
 
 echo Creating config map $APP_NAME-frontend-config-map
 oc create -n $OPENSHIFT_NAMESPACE-$envValue configmap $APP_NAME-frontend-config-map --from-literal=TZ=$TZVALUE --from-literal=HOST_ROUTE=$HOST_ROUTE  --dry-run -o yaml | oc apply -f -
-
+echo
+echo Setting environment variables for $APP_NAME-frontend-$SOAM_KC_REALM_ID application
+oc -n $OPENSHIFT_NAMESPACE-$envValue set env --from=configmap/$APP_NAME-frontend-config-map dc/$APP_NAME-frontend-$SOAM_KC_REALM_ID
 
 
