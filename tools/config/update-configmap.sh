d53116c9e974ec7d3c934f9e4f20d7f8b2578ad1
Shaun Lum <shaun.lum@gov.bc.ca>
2023-09-05 14:03:55 -0700

    added intitute expiry

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -6,6 +6,7 @@ KC_CLIENT_SECRET=$5
 KC_CLIENT_ID=$6
 KC_TOKEN_URL=$7
 
+
 APP_NAME_UPPER=${APP_NAME^^}
 
 TZVALUE="America/Vancouver"
@@ -26,7 +27,8 @@ then
 fi
 
 echo Creating config map $APP_NAME-backend-config-map
-oc create -n $OPENSHIFT_NAMESPACE-$envValue configmap $APP_NAME-backend-config-map --from-literal=TZ=$TZVALUE --from-literal=BANNER_COLOR=$bannerColor --from-literal=BANNER_ENVIRONMENT=$bannerEnvironment --from-literal=SOAM_CLIENT_ID=$KC_CLIENT_SECRET --from-literal=SOAM_TOKEN_URL=$KC_TOKEN_URL --from-literal=SOAM_CLIENT_SECRET=$KC_CLIENT_ID --from-literal=SERVER_FRONTEND="$SERVER_FRONTEND" --from-literal=INSTITUTE_API_URL="https://institute-api-$COMMON_NAMESPACE-$envValue.apps.silver.devops.gov.bc.ca/api/v1" --from-literal=LOG_LEVEL=info  --from-literal=NODE_ENV="openshift" --dry-run -o yaml | oc apply -f -
+oc create -n $OPENSHIFT_NAMESPACE-$envValue configmap $APP_NAME-backend-config-map --from-literal=TZ=$TZVALUE --from-literal=BANNER_COLOR=$bannerColor --from-literal=BANNER_ENVIRONMENT=$bannerEnvironment --from-literal=SOAM_CLIENT_ID=$KC_CLIENT_SECRET --from-literal=SOAM_TOKEN_URL=$KC_TOKEN_URL --from-literal=SOAM_CLIENT_SECRET=$KC_CLIENT_ID --from-literal=SERVER_FRONTEND="$SERVER_FRONTEND" --from-literal=INSTITUTE_API_URL="https://institute-api-$COMMON_NAMESPACE-$envValue.apps.silver.devops.gov.bc.ca/api/v1" 
+--from-literal=INSTITUTE_API_EXPIRY=300 --from-literal=LOG_LEVEL=info  --from-literal=NODE_ENV="openshift" --dry-run -o yaml | oc apply -f -
 echo
 echo Setting environment variables for $APP_NAME-backend-$SOAM_KC_REALM_ID application
 oc -n $OPENSHIFT_NAMESPACE-$envValue set env --from=configmap/$APP_NAME-backend-config-map dc/$APP_NAME-backend-main
