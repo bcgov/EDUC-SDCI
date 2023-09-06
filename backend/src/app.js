5e9bc3d2fad99b8b4afcb0f504ddad4f1569daad
Shaun Lum <shaun.lum@gov.bc.ca>
2023-09-06 10:49:50 -0700

    added back download

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,7 +35,43 @@ app.use(json2xls.middleware);
 app.get('/api/health', (req, res) => {
   res.status(200).send('OK');
 });
+
+app.get('/download/:fileName', (req, res) => {
+  try {
+    // Get the requested file name from the URL parameter
+    const { fileName } = req.params;
+
+    // Construct the file path based on the requested file name
+    
+    const filePath = path.join(__dirname, '../public', fileName);
+    console.log(filePath)
+    // Check if the file exists
+    if (!filePath || !fileName || !fs.existsSync(filePath)) {
+      // If the file doesn't exist, send a 404 Not Found response
+      res.status(404).send('File not found');
+      return;
+    }
+
+    // Set the appropriate headers for a file download
+    res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
+    res.setHeader('Content-Type', 'application/octet-stream'); // Set the appropriate MIME type
+
+    // Send the file as the response
+    res.sendFile(filePath, (err) => {
+      if (err) {
+        // If an error occurs during sending, log the error and send a 500 Internal Server Error response
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+  } catch (err) {
+    // Handle any unexpected errors with a 500 Internal Server Error response
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 app.use(/(\/api)?/, apiRouter);
+
 
 apiRouter.use('/v1/download', downloadRouter);
 apiRouter.use('/v1/institute', instituteRouter);
