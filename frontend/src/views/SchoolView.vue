964da525b7f344d6ea993d1cb193181709276fb9
suzalflueck <samflueck95@gmail.com>
2023-10-10 11:27:47 -0700

    School view changes

--- frontend/src/views/SchoolView.vue
+++ frontend/src/views/SchoolView.vue
@@ -4,6 +4,9 @@ import { useRoute } from 'vue-router'
 import InstituteService from '@/services/InstituteService'
 import { useAppStore } from '@/stores/app'
 import type { School } from '@/types/types.d.ts'
+import { formatPhoneNumber } from '@/utils/common'
+// import common components
+import DisplayAddress from '@/components/common/DisplayAddress.vue'
 const appStore = useAppStore()
 const schoolData = reactive({ value: {} as School })
 const filteredContacts = ref<any>([])
@@ -48,37 +51,72 @@ onBeforeMount(async () => {
     console.error(error)
   }
 })
+
+function downloadSchoolInfo() {
+  alert('TODO - Implement school info extract download')
+}
 </script>
 
 <template>
-  
   <div>
-    <v-breadcrumbs class="breadcrumbs" bg-color="primary" :items="[{title: 'Home', href:'/'}, 'School', schoolData.value.displayName]"></v-breadcrumbs>
-    <v-card class="fill-screen-height"  width="100%" v-if="schoolData.value" >
-      <v-card-item>
+    <v-breadcrumbs
+      class="breadcrumbs"
+      bg-color="white"
+      :items="[{ title: 'Home', href: '/' }, 'School', schoolData.value.displayName]"
+    ></v-breadcrumbs>
+
+    <v-sheet style="z-index: 100; position: relative" elevation="2" class="py-6 full-width">
+      <v-row no-gutters justify="space-between">
+        <v-spacer />
+        <v-col cols="6">
+          <v-row v-if="schoolData.value" no-gutters justify="space-between">
+            <v-col>
+              <v-row>
+                <h2 class="mt-3 mb-2">
+                  {{ schoolData.value.displayName }} - {{ schoolData.value.mincode }}
+                </h2>
+              </v-row>
+              <v-row
+                >District {{ districtInfo.value.districtNumber }} -
+                {{ districtInfo.value.displayName }}</v-row
+              >
+              <v-row>
+                <v-col class="pl-0">
+                  <p>
+                    <strong>Phone:</strong> {{ formatPhoneNumber(schoolData.value.phoneNumber) }}
+                  </p>
+                  <p><strong>Fax:</strong> {{ formatPhoneNumber(schoolData.value.faxNumber) }}</p>
+                  <p>
+                    <strong>Email: </strong>
+                    <a :href="schoolData.value?.email"> {{ schoolData.value.email }} </a>
+                  </p>
+                </v-col>
+                <v-col v-for="item in schoolData.value.addresses" :key="item.addressTypeCode">
+                  <DisplayAddress v-bind="item" />
+                </v-col>
+                <v-col
+                  ><v-btn block class="text-none text-subtitle-1 ma-1" @click="downloadSchoolInfo"
+                    ><template v-slot:prepend> <v-icon icon="mdi-download" /> </template>School
+                    Info</v-btn
+                  ></v-col
+                >
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-spacer />
+      </v-row>
+    </v-sheet>
+    <v-card class="fill-screen-height" width="100%" v-if="schoolData.value">
+      <!-- <v-card-item>
         <v-card-title v-if="schoolData.value.displayName">
           {{ schoolData.value.displayName }} - {{ schoolData.value.mincode }}</v-card-title
         >
         District: {{ districtInfo.value.districtNumber }} - {{ districtInfo.value.displayName }}
 
-        <v-card-subtitle>
-          <!-- School Address -->
-          <div v-if="filteredAddresses.value != 'N/A'">
-            <span v-if="filteredAddresses.value.addressLine1"
-              >{{ filteredAddresses.value.addressLine1 }}<br
-            /></span>
-            <span v-if="filteredAddresses.value.addressLine2"
-              >{{ filteredAddresses.value.addressLine2 }}<br
-            /></span>
-            <span v-if="filteredAddresses.value.city">{{ filteredAddresses.value.city }}</span
-            >,
-            <span v-if="filteredAddresses.value.provinceCode"
-              >{{ filteredAddresses.value.provinceCode }},</span
-            >
-            <span v-if="filteredAddresses.value.postal">{{ filteredAddresses.value.postal }}</span>
-          </div>
-          <!-- School type info. -->
-          <span v-if="schoolData.value.facilityTypeCode"
+        <v-card-subtitle> -->
+      <!-- School type info. -->
+      <!-- <span v-if="schoolData.value.facilityTypeCode"
             >{{ schoolData.value.facilityTypeCode }} SCHOOL<br
           /></span>
           <div v-if="schoolData.value.grades">
@@ -92,7 +130,7 @@ onBeforeMount(async () => {
             >{{ schoolData.value.schoolCategoryCode }} SCHOOL</span
           >
         </v-card-subtitle>
-      </v-card-item>
+      </v-card-item> -->
       <v-card-text>
         <v-data-table-virtual
           :headers="headers"
