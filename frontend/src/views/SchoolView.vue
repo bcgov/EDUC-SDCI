ce0c06983d1f4555ee2e7a6d370b0d35f8731850
Tang <Michael.Tang@gov.bc.ca>
2023-10-11 14:15:18 -0700

    added school download

--- frontend/src/views/SchoolView.vue
+++ frontend/src/views/SchoolView.vue
@@ -4,11 +4,14 @@ import { useRoute } from 'vue-router'
 import InstituteService from '@/services/InstituteService'
 import { useAppStore } from '@/stores/app'
 import type { School } from '@/types/types.d.ts'
+import * as jsonexport from 'jsonexport/dist'
+import { distNumberFromMincode } from '@/utils/common'
+// props
 const appStore = useAppStore()
-const schoolData = reactive({ value: {} as School })
+const districtInfo = reactive<any>({ value: {} })
+const downloadContacts = ref<any>([])
 const filteredContacts = ref<any>([])
 const filteredAddresses = reactive<any>({ value: {} })
-const districtInfo = reactive<any>({ value: {} })
 const headers = [
   { title: 'Contact', key: 'jobTitle' },
   { title: 'First Name', key: 'firstName' },
@@ -18,12 +21,63 @@ const headers = [
   { title: 'Fax', key: 'faxNumber' },
   { title: 'Email', key: 'email' }
 ]
+const schoolData = reactive({ value: {} as School })
+
+// functions
+const downloadCSV = () => {
+  jsonexport(downloadContacts.value, function (err: any, csv: any) {
+    if (err) return console.error(err)
+    appStore.exportCSV(csv)
+  })
+}
+const transformContactForDownload = (inputData: any) => {
+  return inputData.map(
+    (item: {
+      districtNumber: string
+      mincode: string
+      displayName: string
+      addressLine1: string
+      city: string
+      provinceCode: string
+      postal: string
+      jobTitle: string
+      firstName: string
+      lastName: string
+      facilityTypeCode: string
+      schoolCategoryCode: string
+      phoneNumber: string
+      phoneExtension: string
+      alternatePhoneNumber: string
+      alternatePhoneExtension: string
+      email: string
+    }) => ({
+      districtNumber: item.districtNumber,
+      mincode: item.mincode,
+      displayName: item.displayName,
+      addressLine1: item.addressLine1,
+      city: item.city,
+      provinceCode: item.provinceCode,
+      postal: item.postal,
+      jobTitle: item.jobTitle,
+      firstName: item.firstName,
+      lastName: item.lastName,
+      facilityTypeCode: item.facilityTypeCode,
+      schoolCategoryCode: item.schoolCategoryCode,
+      phoneNumber: item.phoneNumber,
+      phoneExtension: item.phoneExtension,
+      alternatePhoneNumber: item.alternatePhoneNumber,
+      alternatePhoneExtension: item.alternatePhoneExtension,
+      email: item.email
+    })
+  )
+}
 onBeforeMount(async () => {
   const route = useRoute()
   const selectedSchoolId: string | string[] = route.params.schoolId
   try {
     const response = await InstituteService.getSchool(selectedSchoolId as string)
     schoolData.value = response.data
+    console.log(schoolData.value)
     //setting district name and number
     if (schoolData.value.districtId) {
       districtInfo.value = appStore.getDistrictByDistrictId(String(schoolData.value.districtId))
@@ -38,10 +92,22 @@ onBeforeMount(async () => {
     if (response.data) {
       if (response.data.contacts.length > 0) {
         filteredContacts.value = response.data.contacts
+        filteredContacts.value[0].districtNumber = distNumberFromMincode(response.data.mincode)
+        filteredContacts.value[0].displayName = response.data.displayName
+        filteredContacts.value[0].schoolCategoryCode = response.data.schoolCategoryCode
+        filteredContacts.value[0].facilityTypeCode = response.data.facilityTypeCode
+        filteredContacts.value[0].mincode = response.data.mincode
         filteredContacts.value[0].phoneNumber = response.data.phoneNumber
         filteredContacts.value[0].phoneExtension = response.data.phoneExtension
         filteredContacts.value[0].email = response.data.email
         filteredContacts.value[0].faxNumber = response.data.faxNumber
+        filteredContacts.value[0].addressLine1 = response.data.addresses[0].addressLine1
+        filteredContacts.value[0].addressLine2 = response.data.addresses[0].addressLine2
+        filteredContacts.value[0].city = response.data.addresses[0].city
+        filteredContacts.value[0].provinceCode = response.data.addresses[0].provinceCode
+        filteredContacts.value[0].countryCode = response.data.addresses[0].countryCode
+        filteredContacts.value[0].postal = response.data.addresses[0].postal
+        downloadContacts.value = transformContactForDownload(filteredContacts.value)
       }
     }
   } catch (error) {
@@ -51,16 +117,18 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  
   <div>
-    <v-breadcrumbs class="breadcrumbs" bg-color="primary" :items="[{title: 'Home', href:'/'}, 'School', schoolData.value.displayName]"></v-breadcrumbs>
-    <v-card class="fill-screen-height"  width="100%" v-if="schoolData.value" >
+    <v-breadcrumbs
+      class="breadcrumbs"
+      bg-color="primary"
+      :items="[{ title: 'Home', href: '/' }, 'School', schoolData.value.displayName]"
+    ></v-breadcrumbs>
+    <v-card class="fill-screen-height" width="100%" v-if="schoolData.value">
       <v-card-item>
         <v-card-title v-if="schoolData.value.displayName">
           {{ schoolData.value.displayName }} - {{ schoolData.value.mincode }}</v-card-title
         >
         District: {{ districtInfo.value.districtNumber }} - {{ districtInfo.value.displayName }}
-
         <v-card-subtitle>
           <!-- School Address -->
           <div v-if="filteredAddresses.value != 'N/A'">
@@ -94,6 +162,14 @@ onBeforeMount(async () => {
         </v-card-subtitle>
       </v-card-item>
       <v-card-text>
+        <v-btn
+          @click="downloadCSV"
+          class="text-none text-subtitle-1 ma-1"
+          variant="flat"
+          :disabled="!schoolData.value"
+        >
+          <template v-slot:prepend> <v-icon icon="mdi-download" /> </template>Download to CSV
+        </v-btn>
         <v-data-table-virtual
           :headers="headers"
           :items="filteredContacts"
