250a3d08fa0008fef4916d084e64c5a93618e1f5
Tang <Michael.Tang@gov.bc.ca>
2023-11-16 10:48:50 -0800

    download errors fix

--- frontend/src/views/AuthorityView.vue
+++ frontend/src/views/AuthorityView.vue
@@ -5,7 +5,7 @@ import router from '@/router'
 import { useAppStore } from '@/stores/app'
 import { useRoute } from 'vue-router'
 import { formatPhoneNumber } from '@/utils/common'
-import * as jsonexport from 'jsonexport/dist'
+import jsonexport from 'jsonexport/dist'
 import type { Authority, School, Address } from '@/types/types.d.ts'
 import { useSanitizeURL } from '@/composables/string'
 
