234912173b53359b2c5c67d57e056df2b3d4d158
Shaun Lum <shaun.lum@gov.bc.ca>
2023-11-24 15:47:35 -0800

    updated cronjobs

--- backend/src/components/cache.js
+++ backend/src/components/cache.js
@@ -2,7 +2,7 @@ const NodeCache = require("node-cache");
 const listCache = new NodeCache({ stdTTL: 21600 });
 const schoolCache = new NodeCache({ stdTTL: 21600 });
 const codeCache = new NodeCache({ stdTTL: 21600 });
-const fileCache = new NodeCache({ stdTTL: 21600 });
+const fileCache = new NodeCache({ stdTTL: 39600 }); 
 
 module.exports = {
     listCache,schoolCache,codeCache, fileCache
