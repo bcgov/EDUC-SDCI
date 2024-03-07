da290d9474592bd3482960c9cb9af57c56838b08
Shaun Lum <shaun.lum@gov.bc.ca>
2024-03-07 14:52:59 -0800

    updated banner message

--- frontend/src/components/common/DisplayAlert.vue
+++ frontend/src/components/common/DisplayAlert.vue
@@ -4,8 +4,10 @@
   <v-alert type="info" color="primary" variant="outlined" style="background-color: #f6f9fc"
     >The information on this page is provided by schools and districts through the Education Data
     Exchange (EDX).
-    <a href="https://www2.gov.bc.ca/gov/content?id=0F6C7869EEC844F190091E71D6BA3B58"
-      >Learn more.</a
-    ></v-alert
-  >
+    <a href="https://www2.gov.bc.ca/gov/content?id=0F6C7869EEC844F190091E71D6BA3B58">Learn more.</a>
+    <br />If you represent an Independent School or Independent Authority and have not yet been
+    onboarded to EDX, please contact the
+    <a href="mailto:EDUC.independentSchoolsOffice@gov.bc.ca">Independent Schools Office</a>
+    to update your contact information.
+  </v-alert>
 </template>
