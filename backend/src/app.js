33147af1ff80d061eed4fa3189a06ea7db11391e
Shaun Lum <shaun.lum@gov.bc.ca>
2025-12-12 14:32:17 -0800

    updated caddyfile

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,8 +21,6 @@ app.get("/download/*", (req, res) => {
   try {
     const requestedFile = req.params[0];
     const filePath = path.resolve(publicPath, requestedFile);
-    log.info("publicPath:", publicPath);
-    log.info("filePath" + filePath);
     // Security check
     if (!filePath.startsWith(publicPath)) {
       return res.status(403).send("Forbidden");
