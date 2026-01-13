c03ac11d766703ead3c1d4e595c3ae1c5531e44b
Shaun Lum <shaun.lum@gov.bc.ca>
2026-01-13 11:39:33 -0800

    fixed filename

--- backend/src/components/cache-service.js
+++ backend/src/components/cache-service.js
@@ -1059,7 +1059,7 @@ const cacheService = {
       // All Schools Mailing
       const filePathAllSchoolsMailing = path.join(
         FILE_STORAGE_DIR,
-        "allschoolMailing.csv"
+        "allschoolmailing.csv"
       );
       const propertyOrderAllSchools = [
         { property: "districtNumber", label: "District Number" },
--- frontend/src/views/DistrictView.vue
+++ frontend/src/views/DistrictView.vue
@@ -151,7 +151,7 @@ async function getDistrictData(): Promise<void> {
           return {
             'District Number': response.data.districtData?.districtNumber,
             'District Name': response.data.districtData?.displayName,
-            'Contact Type': item.label,
+            'Contact Type': item.districtContactLabel,
             'Job Title': item.jobTitle,
             'First Name': item.firstName,
             'Last Name': item.lastName,
@@ -183,19 +183,15 @@ onMounted(async () => {
 
 <template>
   <div>
-    <v-breadcrumbs
-      class="breadcrumbs"
-      bg-color="white"
-      :items="[
-        { title: 'Home', href: '/' },
-        'District',
-        district.value.districtData
-          ? district.value.districtData.districtNumber +
-            ' ' +
-            district.value.districtData.displayName
-          : ''
-      ]"
-    ></v-breadcrumbs>
+    <v-breadcrumbs class="breadcrumbs" bg-color="white" :items="[
+      { title: 'Home', href: '/' },
+      'District',
+      district.value.districtData
+        ? district.value.districtData.districtNumber +
+        ' ' +
+        district.value.districtData.displayName
+        : ''
+    ]"></v-breadcrumbs>
 
     <v-sheet style="z-index: 100; position: relative" elevation="2" class="py-6 full-width">
       <v-container id="main">
@@ -228,8 +224,7 @@ onMounted(async () => {
                   {{ formatPhoneNumber(district.value.districtData?.faxNumber) }}
                 </p>
                 <p v-if="district.value.districtData?.email">
-                  <strong>Email: </strong
-                  ><a :href="'mailto:' + district.value.districtData?.email">{{
+                  <strong>Email: </strong><a :href="'mailto:' + district.value.districtData?.email">{{
                     district.value.districtData?.email
                   }}</a>
                 </p>
@@ -240,30 +235,20 @@ onMounted(async () => {
                 </p>
               </v-col>
 
-              <v-col
-                cols="11"
-                md="auto"
-                v-for="item in district.value.districtData.addresses"
-                :key="item.addressTypeCode"
-              >
+              <v-col cols="11" md="auto" v-for="item in district.value.districtData.addresses"
+                :key="item.addressTypeCode">
                 <DisplayAddress v-bind="item" class="mb-3" />
               </v-col>
 
               <v-col cols="11" md="4" class="pa-0 pa-md-3">
-                <v-btn
-                  variant="text"
-                  class="text-none text-subtitle-1 ma-1 v-btn-align-left"
-                  @click="downloadDistrictContacts"
-                  ><template v-slot:prepend> <v-icon icon="mdi-download" /> </template>Download
-                  District Contacts (CSV)</v-btn
-                >
-                <v-btn
-                  variant="text"
-                  class="text-none text-subtitle-1 ma-1 v-btn-align-left"
-                  @click="downloadDistrictSchools"
-                  ><template v-slot:prepend> <v-icon icon="mdi-download" /> </template>Download
-                  District Schools (CSV)</v-btn
-                >
+                <v-btn variant="text" class="text-none text-subtitle-1 ma-1 v-btn-align-left"
+                  @click="downloadDistrictContacts"><template v-slot:prepend> <v-icon icon="mdi-download" />
+                  </template>Download
+                  District Contacts (CSV)</v-btn>
+                <v-btn variant="text" class="text-none text-subtitle-1 ma-1 v-btn-align-left"
+                  @click="downloadDistrictSchools"><template v-slot:prepend> <v-icon icon="mdi-download" />
+                  </template>Download
+                  District Schools (CSV)</v-btn>
               </v-col>
             </v-row>
           </v-col>
@@ -292,20 +277,10 @@ onMounted(async () => {
         <v-window v-model="tab">
           <!-- District Contacts tab contents -->
           <v-window-item :value="tabOptions.contacts">
-            <v-text-field
-              v-model="contactSearch"
-              append-icon="mdi-magnify"
-              label="Filter District Contacts"
-              single-line
-              hide-details
-            ></v-text-field>
-            <v-data-table
-              items-per-page="-1"
-              :headers="contactHeaders"
-              :items="district.value.districtData?.contacts"
-              :search="contactSearch"
-              :sort-by="[{ key: 'label', order: 'asc' }]"
-            >
+            <v-text-field v-model="contactSearch" append-icon="mdi-magnify" label="Filter District Contacts" single-line
+              hide-details></v-text-field>
+            <v-data-table items-per-page="-1" :headers="contactHeaders" :items="district.value.districtData?.contacts"
+              :search="contactSearch" :sort-by="[{ key: 'label', order: 'asc' }]">
               <template v-slot:item.email="{ item }">
                 <div style="max-width: 250px; overflow: hidden">
                   <a :href="`mailto:${item.email}`">{{ item.email }}</a>
@@ -321,20 +296,11 @@ onMounted(async () => {
           </v-window-item>
           <!-- District Schools tab contents -->
           <v-window-item :value="tabOptions.schools">
-            <v-text-field
-              v-model="schoolSearch"
-              append-icon="mdi-magnify"
-              label="Filter District Schools"
-              single-line
-              hide-details
-            ></v-text-field>
-            <v-data-table
-              items-per-page="-1"
-              :headers="schoolHeaders"
-              :items="district.value.districtData.districtSchools"
-              :search="schoolSearch"
-              :sort-by="[{ key: 'mincode', order: 'asc' }]"
-            >
+            <v-text-field v-model="schoolSearch" append-icon="mdi-magnify" label="Filter District Schools" single-line
+              hide-details></v-text-field>
+            <v-data-table items-per-page="-1" :headers="schoolHeaders"
+              :items="district.value.districtData.districtSchools" :search="schoolSearch"
+              :sort-by="[{ key: 'mincode', order: 'asc' }]">
               <template v-slot:item.displayName="{ item }">
                 <a @click="goToSchool(item.displayName, item.mincode, item.schoolId)">{{
                   item.displayName
