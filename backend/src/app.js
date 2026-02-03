dfd47dc3f5b4463d9cfc3fa705c7578bbe1beabb
Shaun Lum <shaun.lum@gov.bc.ca>
2026-02-03 14:42:26 -0800

    added nats

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,14 +3,13 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const log = require("./components/logger");
-const cors = require("cors");
-const NodeCache = require("node-cache");
 const apiRouter = express.Router();
 const codesRouter = require("./routes/codes-router");
 const districtRouter = require("./routes/district-router");
 const authorityRouter = require("./routes/authority-router");
 const schoolRouter = require("./routes/school-router");
 const searchRouter = require("./routes/search-router");
+const cacheService = require("./components/cache-service");
 const app = express();
 const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
@@ -46,7 +45,11 @@ app.get("/api/download/*", (req, res) => {
 });
 
 app.get("/api/health", (req, res) => {
-  res.status(200).send("OK");
+  res.status(200).json({
+    status: "OK",
+    lastCacheRebuild: cacheService.getLastCacheRebuild(),
+    uptime: process.uptime(),
+  });
 });
 
 app.use("/api", apiRouter);
